b47e66ea64e4499e1551a6cd9cccaf0a4f36656b
Ariana Bray <aribray@gmail.com>
2019-04-02 08:25:34 -0700

    refactored reverse_sentence

--- lib/reverse_sentence.rb
+++ lib/reverse_sentence.rb
@@ -1,6 +1,15 @@
 # A method to reverse the words in a sentence, in place.
-# Time complexity: ?
-# Space complexity: ?
+# Time complexity: O(n)
+# Space complexity: O(1)
+
+# combination of reverse_words and string_reverse alorithms from before
+require 'pry'
+require_relative '../../reverse_words/lib/reverse_words'
+require_relative '../../string_reverse/lib/string_reverse'
 def reverse_sentence(my_sentence)
-  raise NotImplementedError
+  # first reverse letters in the words ex. "Hello World!" becomes "olleH !dlroW"
+  reverse_words(my_sentence)
+
+  # next, reverse the whole string, ex. "olleH !dlroW" becomes "World! Hello"
+  string_reverse(my_sentence)
 end
